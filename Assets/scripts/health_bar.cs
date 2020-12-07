a155a95d1a6dacb663a9847bda72b186d7e80f88
Dan <daniel.bedford56@gmail.com>
2020-12-07 12:23:46 +0000

    Built 1st map

---
+++ Assets/scripts/health_bar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class health_bar : MonoBehaviour
+{
+    [SerializeField]
+    private Slider health_slider;
+
+    public void set_health(int health)
+    {
+        health_slider.value = health;
+    }
+}
