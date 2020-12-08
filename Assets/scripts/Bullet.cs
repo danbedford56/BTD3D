23f2f2809e466226ae365e3782ec201c635e9fd4
Dan <daniel.bedford56@gmail.com>
2020-12-08 16:23:39 +0000

    Added missile launcher

--- Assets/scripts/Bullet.cs
+++ Assets/scripts/Bullet.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [Header("Bullet Attributes:")]
     private Transform target;
     public float bulletSpeed = 80f;
+    public float explosionRadius = 0f;
+
+    [Header("Bullet Setup:")]
+    public GameObject impactEffect;
+    [SerializeField]
+    private string enemyTag = "Enemy";
+
     [HideInInspector]
     public float damage;
 
@@ -31,11 +39,41 @@ public class Bullet : MonoBehaviour
             return;
         }
         transform.Translate(direction.normalized * distanceOnUpdate, Space.World);
+        transform.LookAt(target);
     }
 
     void HitTarget()
     {
-        target.GetComponent<Enemy>().takeDamage(damage);
+        GameObject effectInstance = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effectInstance, 1f);
+
+        if (explosionRadius > 0f)
+        {
+            // Explody stuff
+            Explode();
+        } else {
+            // Non-explody stuff
+            target.GetComponent<Enemy>().takeDamage(damage);
+        }
+
         Destroy(gameObject);
+    }
+
+    void Explode()
+    {
+        Collider[] collidersHit = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider collider in collidersHit)
+        {
+            if (collider.tag == enemyTag)
+            {
+                collider.GetComponent<Enemy>().takeDamage(damage);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
