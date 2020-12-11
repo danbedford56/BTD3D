3b57ca5732d3c1dc2b8f5e62e0f4ee3233850488
Dan <daniel.bedford56@gmail.com>
2020-12-11 11:10:26 +0000

    Setup nature

---
+++ Assets/scripts/Nature.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nature : MonoBehaviour
+{
+    public int costToDestroy;
+    public Vector3 positionOffset;
+
+    public void DestroyNature()
+    {
+        PlayerStatus.monees -= costToDestroy;
+    }
+}
