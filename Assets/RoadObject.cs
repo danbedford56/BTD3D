cfa401e8cf41c2baf4d1e7ade451f2a17d443675
Dan <daniel.bedford56@gmail.com>
2020-12-15 16:24:32 +0000

    Added sanitiser auto rotation

--- Assets/RoadObject.cs
+++ Assets/RoadObject.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class RoadObject : MonoBehaviour
 {
+    private float sanitiserCountdown = 5f;
+
+    private void Update()
+    {
+        if (this.gameObject.activeSelf)
+        {
+            if (sanitiserCountdown <= 0f)
+            {
+                this.gameObject.SetActive(false);
+                sanitiserCountdown = 5f;
+            }
+            sanitiserCountdown -= Time.deltaTime;
+        }
+    }
+    
     private void OnTriggerEnter(Collider enemy)
     {
         if (enemy.GetComponent<Enemy>())
