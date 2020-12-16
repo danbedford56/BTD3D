e2a4ed38d6e5c19112ff77aefe53ad144586d326
Jack <jack3009@hotmail.co.uk>
2020-12-16 12:11:10 +0000

    Level 2 completed

---
+++ Assets/winningGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class winningGame : MonoBehaviour
+{
+    public string pageToLoad = "Main Menu";
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(pageToLoad);
+        Debug.Log("Got to Menu.");
+    }
+
+}
